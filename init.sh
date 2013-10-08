d292821ac086cef6a65e655bce1234464c4062af
Jingyi Ni <colonel.ni.jingyi@gmail.com>
2013-10-07 22:09:09 -0400

    added sbtconfig for sbt startup

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@ ln -s -f $HOME/dotfiles/vimrc $HOME/.vimrc
 ln -s -f $HOME/dotfiles/bashrc $HOME/.bashrc 
 ln -s -f $HOME/dotfiles/gitconfig $HOME/.gitconfig
 ln -s -f $HOME/dotfiles/bash_profile $HOME/.bash_profile
+ln -s -f $HOME/dotfiles/sbtconfig $HOME/.sbtconfig
 
 source $HOME/.bashrc
